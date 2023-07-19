5751ca7a74f0173dda8a9fc4e6f36c738621c38d
Harmelodic <matt@harmelodic.com>
2023-07-19 21:36:00 +0200

    Basic frame of an application

--- src/main/java/com/harmelodic/gamelauncher/Main.java
+++ src/main/java/com/harmelodic/gamelauncher/Main.java
@@ -3,7 +3,7 @@ package com.harmelodic.gamelauncher;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,13 +13,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/Root.fxml"));
-        VBox page = loader.load();
+        BorderPane page = loader.load();
         Scene scene = new Scene(page);
         stage.setScene(scene);
 
         stage.setTitle("Game Launcher");
-        stage.setWidth(1000);
-        stage.setHeight(900);
+        stage.setWidth(1280);
+        stage.setHeight(960);
         stage.show();
     }
 
--- src/main/java/com/harmelodic/gamelauncher/RootController.java
+++ src/main/java/com/harmelodic/gamelauncher/RootController.java
@@ -1,18 +1,35 @@
 package com.harmelodic.gamelauncher;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class RootController {
+    @FXML
+    public BorderPane pane;
 
     @FXML
-    public Pane mainPane;
+    public ToolBar toolbar;
+
+    @FXML
+    public VBox gameList;
+
+    @FXML
+    public VBox filters;
+
+    @FXML
+    public VBox statusBar;
 
     @FXML
     private void initialize() {
-        Label label = new Label();
-        label.setText("Welcome to Game Launcher");
-        mainPane.getChildren().setAll(label);
+        toolbar.getItems().addAll(
+                new TextField("Search for a game..."),
+                new Button("Search")
+        );
+        Platform.runLater(() -> pane.requestFocus());
     }
 }
