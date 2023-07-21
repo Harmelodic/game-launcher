812eea29769a834199abe00a1ff62dd37efd3c3e
Harmelodic <matt@harmelodic.com>
2023-07-22 00:40:35 +0200

    jlink didn't work in GitHub Actions, remove for now

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module gamelauncher {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.harmelodic.gamelauncher.ui to javafx.fxml;
-    opens com.harmelodic.gamelauncher.ui.settings to javafx.fxml;
-
-    exports com.harmelodic.gamelauncher;
-}
