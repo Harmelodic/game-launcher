592b78b28edb7cb12ad846e475f2b927a1a535c1
Harmelodic <matt@harmelodic.com>
2023-07-21 22:03:02 +0200

    Cleanup

--- src/main/java/com/harmelodic/gamelauncher/ui/ToolbarController.java
+++ src/main/java/com/harmelodic/gamelauncher/ui/ToolbarController.java
@@ -1,16 +1,12 @@
 package com.harmelodic.gamelauncher.ui;
 
 import com.harmelodic.gamelauncher.ui.settings.SettingsStage;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
-import javafx.stage.Stage;
-
-import java.util.Set;
 
 public class ToolbarController {
     @FXML
