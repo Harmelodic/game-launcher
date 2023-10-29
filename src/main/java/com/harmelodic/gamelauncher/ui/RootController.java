28171c8b0ba5c55bef2002cc3ebb16af910e140e
Matt Smith <matt@harmelodic.com>
2023-10-29 16:14:20 +0100

    Read strings from resource file rather than hardcode (#9)

--- src/main/java/com/harmelodic/gamelauncher/ui/RootController.java
+++ src/main/java/com/harmelodic/gamelauncher/ui/RootController.java
@@ -1,9 +1,11 @@
 package com.harmelodic.gamelauncher.ui;
 
+import com.harmelodic.gamelauncher.App;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -19,12 +21,13 @@ public class RootController {
     public VBox filters;
 
     @FXML
-    public VBox statusBar;
+    public HBox statusBar;
 
     @FXML
     public void initialize() throws IOException {
         Platform.runLater(() -> pane.requestFocus());
 
-        pane.setTop(new FXMLLoader(getClass().getResource("/Toolbar.fxml")).load());
+        FXMLLoader toolbarLoader = new FXMLLoader(getClass().getResource("/Toolbar.fxml"), App.STRINGS);
+        pane.setTop(toolbarLoader.load());
     }
 }
